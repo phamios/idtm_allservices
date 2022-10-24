050d3976ce479ecd6fd42d1d5fe2e812955ffa44
Mac Os <macos@MacBook-Pro-cua-Mac-2.local>
2022-10-24 12:22:54 +0700

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/entities/user.entity';
-import { AuthenticationController } from './authentication/authentication.controller';
-import { AuthenticationModule } from './authentication/authentication.module';
-import { AuthenticationService } from './authentication/authentication.service';
-
+ 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -22,11 +19,10 @@ import { AuthenticationService } from './authentication/authentication.service';
       // entities: [UsersEntity],
       synchronize: true,
     }),
-    UsersModule,
-    AuthenticationModule,
+    UsersModule, 
   ],
-  controllers: [AppController, AuthenticationController],
-  providers: [AppService, AuthenticationService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {
   // constructor(private dataSource: DataSource) {}
